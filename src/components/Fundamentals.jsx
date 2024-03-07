9be83dbd6e3c145f2522b1b87afe484d957c65db
Rahul Meena <rahul.meena9695@gmail.com>
2024-03-07 22:13:58 +0530

    UI updated

--- src/components/Fundamentals.jsx
+++ src/components/Fundamentals.jsx
@@ -2,16 +2,17 @@ import Performance from "./Performance";
 
 const Fundamentals = () => {
   return (
-    <div className="md:mt-8">
-      <div className="flex flex-col">
-        <ul className="hidden md:flex mx-4 justify-between font-semibold text-gray-600">
-          <li className="underline-offset-[17px] underline cursor-pointer">Overview</li>
-          <li className="cursor-pointer">Fundamentals</li>
-          <li className="cursor-pointer">News Insights</li>
-          <li className="cursor-pointer">Sentiments</li>
-          <li className="cursor-pointer">Team</li>
-          <li className="cursor-pointer">Technicals</li >
-          <li className="cursor-pointer">Tokenomics</li>
+    <div className="mt-8">
+      <div className="flex flex-col relative">
+        <ul className=" overflow-y-scroll scrollbar-hide text-center flex mx-4 gap-[40px] font-semibold text-gray-600">
+          <div className="w-[110px] h-[2px] bg-gray-500 absolute top-[35px] rounded-md"></div>
+          <li className="cursor-pointer min-w-[110px]">Overview</li>
+          <li className="cursor-pointer min-w-[110px]">Fundamentals</li>
+          <li className="cursor-pointer min-w-[110px]">News Insights</li>
+          <li className="cursor-pointer min-w-[110px]">Sentiments</li>
+          <li className="cursor-pointer min-w-[110px]">Team</li>
+          <li className="cursor-pointer min-w-[110px]">Technicals</li >
+          <li className="cursor-pointer min-w-[110px]">Tokenomics</li>
         </ul>
         <hr className="max-w-[700px] my-3 border-gray-300"/>
         <Performance />
